c958e6bd08fb50ac21012b7e5b273383cc93571a
Ben Strycker <bgstrycker1971@gmail.com>
2024-05-06 20:05:02 -0400

    Some updates...

---
+++ DeliveryServiceRepo/DeliveryService.cs
@@ -0,0 +1,29 @@
+namespace DeliveryServiceRepo.repository;
+
+
+
+//This is the repository for the past deliveries with the POCOs...
+ public class DeliveryService
+{
+    public DateTime OrderDate { get; set;}
+
+    public DateTime DeliveryDate { get; set; }
+
+    public string OrderStatus { get; set;}
+
+    public double ItemNumber { get; set; }
+    public double ItemQuantity { get; set;}
+
+    public double CustomerId { get; set;}
+    public Deliveries() []
+    
+    public Deliveries(DateTime OrderDate, DateTime DeliveryDate, string OrderSatus, double ItemNumber, double ItemQuantity, double CustomerId)
+    { 
+        OrderDate = OrderDate;
+        DeliveryDate = DeliveryDate;
+        OrderStatus = OrderStatus;
+        ItemNumber = ItemNumber;
+        ItemQuantity = ItemQuantity;
+        CustomerId = CustomerId;
+    }
+}
--- DeliveryServiceRepo/DeliveryServiceRepo.cs
+++ DeliveryServiceRepo/DeliveryServiceRepo.cs
@@ -1,6 +1,31 @@
-﻿namespace DeliveryServiceRepo.Repository;
+﻿using DeliveryServiceRepo.repository;
 
-public class Class1
+namespace DeliveryServiceRepo.Repository;
+
+public class DeliveryServiceRepo
 {
+    private List<DeliveryService> _listOfDeliveries = new List<DeliveryService>();
 
+    // Create Delivery...
+    public void AddDeliveryToList(DeliveryService deliveryService)
+    {
+        _deliveryservice.Add(deliveryService);
+    }
+    // Read a delivery...
+    public List<DeliveryService> GetDeliveryList(DeliveryService);
+    {
+        return new List<DeliveryService>(_listOfDeliveries);
+    }
+    // Update deliveries...
+    public bool UpdateDeliveryList(DeliveryService deliveryService);
+
+        //Find that delivery content...
+        DeliveryService oldContent = GetDeliveryList();
+
+        // ...And then we'll update that delivery content...
+        
+    //Delete those deliveries...
+
+
+    //Helper Methods...
 }
